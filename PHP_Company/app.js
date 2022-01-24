5afce860e25b14cf3685474b09b509689441af73
Munkh-erdene <menkhtaivan@miu.edu>
2022-01-23 19:27:24 -0600

    added CORS

--- PHP_Company/app.js
+++ PHP_Company/app.js
@@ -17,6 +17,13 @@ app.use(function(req, res, next) {
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.use("/api", function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "PATCH, POST, GET, DELETE");
+    next();
+});
+
 app.use("/api", routes);
 
 // Starting server
